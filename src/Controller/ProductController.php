1c78762051474ea6525acf90c48f127f5a5ade60
Faure Maxime <maxime.faure@cgi.com>
2023-10-26 07:38:47 +0200

    [ADD] - Création Panier

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
+use App\Entity\Product;
+use App\Form\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,12 +22,36 @@ class ProductController extends AbstractController
     }
 
     #[Route('/nos-produits', name: 'app_products')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $products = $this->entityManager->getRepository(Product::class)->findAll();
+
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        } else {
+            $products = $this->entityManager->getRepository(Product::class)->findAll();
+        }
 
         return $this->render('product/index.html.twig', [
             'products' => $products,
+            'form' => $form->createView()
+        ]);
+    }
+    #[Route('/produit/{slug}', name: 'app_product')]
+    public function show($slug): Response
+    {
+
+        $product = $this->entityManager->getRepository(Product::class)->findOneBySlug($slug);
+        if (!$product) {
+            return $this->redirectToRoute('app_products');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
         ]);
     }
 }
